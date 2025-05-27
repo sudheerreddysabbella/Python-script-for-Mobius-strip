f7174c7da80010787a7193128b479a188570b8f8
Sudheer <121391508+sudheerreddysabbella@users.noreply.github.com>
2025-05-27 16:00:52 +0530

    Update mobius.py

--- mobius.py
+++ mobius.py
@@ -124,11 +124,3 @@ class MobiusStrip:
             margin=dict(l=30, r=30, b=30, t=60),
         )
         fig.show()
-
-if __name__ == '__main__':
-    #using the class
-    strip = MobiusStrip(R=1.0, w=0.4, n=150)
-    strip.plot()  # accept 87 diff gradient color scheme thorugh color parameter, refer the readme.md
-    print("Surface Area:", strip.compute_surface_area())  # use fix parameter to fix the decimals count, for example fix=6 => result {x.xxxxxx}
-    print("Edge Length:", strip.compute_edge_length())  # use fix parameter to round the decimal, for example fix=4 => result {x.xxxx}
-    # by default fix is 14 and color is Greys
